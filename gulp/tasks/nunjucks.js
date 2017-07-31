126bb525e81cdc32b2e98438d163f85ef73fd941
Валерий <kaktuz34@gmail.com>
2017-07-31 23:33:14 +0300

    add fonts

--- gulp/tasks/nunjucks.js
+++ gulp/tasks/nunjucks.js
@@ -59,7 +59,7 @@ function renderHtml(onlyChanged) {
                 })
                 // var current = JSON.parse(fs.readFileSync("src/content/"+lang.name+"/home/"+pagename+".json"));
                 langdata = _.extend(langdata, global);
-                console.log(langdata);
+                // console.log(langdata);
                 return langdata;
             }))
             .pipe(nunjucksRender({
